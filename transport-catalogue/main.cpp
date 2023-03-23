c09f631b0fdcc78457d7cd0ad5183e147d4e1f0c
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-03-23 16:40:24 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,15 @@
-// место для вашего кода
+#include <iostream>
+
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+using namespace std;
+using namespace transport_catalogue;
+
+int main() {
+	TransportCatalogue transport_catalogue;
+	input_reader::from_char_stream::ReadDB(transport_catalogue, cin);
+	stat_reader::GetInfo(transport_catalogue);
+	return 0;
+}
