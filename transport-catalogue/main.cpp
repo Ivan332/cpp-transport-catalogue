129755de819ecaa744ee0953f8b755c517d1532d
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-03-24 02:24:04 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -10,6 +10,9 @@ using namespace transport_catalogue;
 int main() {
 	TransportCatalogue transport_catalogue;
 	input_reader::from_char_stream::ReadDB(transport_catalogue, cin);
-	stat_reader::GetInfo(transport_catalogue);
+
+	stat_reader::to_char_stream::StatsPrinter printer(cout);
+	stat_reader::from_char_stream::StatsRequestProcessor Out(cin);
+	Out.ProcessRequests(transport_catalogue, printer);
 	return 0;
 }
