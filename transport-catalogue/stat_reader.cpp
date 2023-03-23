c23291e515c018bb9c1eb4c29646687ad00ae6b2
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-03-24 02:32:37 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -39,61 +39,6 @@ namespace transport_catalogue::stat_reader {
 
         }  // namespace detail
 
-        //// парсит запросы и выводит результат
-        //void GetInfo(const TransportCatalogue& transport_catalogue) {
-        //    int req_count;
-        //    cin >> req_count;
-        //    vector<string> requests;
-        //    do {
-        //        string reqest;
-        //        getline(cin, reqest);
-        //        if (!reqest.empty()) {
-        //            requests.push_back(reqest);
-        //        }
-        //    } while (req_count--);
-        //    for (string& reqest : requests)
-        //    {
-        //        auto cmd = from_char_stream::detail::SplitIntoCommand(reqest);
-        //        if (cmd.first == "Bus"s) {
-        //            string bus_name = cmd.second;
-        //            const auto bus_stats = transport_catalogue.GetBusStats(bus_name);
-        //            cout << "Bus " << bus_name << ": ";
-        //            if (bus_stats.has_value()) {
-        //                assert(bus_stats->crow_route_length > 0);
-        //                auto old_precision = cout.precision();
-        //                cout << setprecision(6) << bus_stats->stops_count << " stops on route, "
-        //                    << bus_stats->unique_stops_count << " unique stops, "
-        //                    << bus_stats->route_length << " route length, "
-        //                    << (bus_stats->route_length / bus_stats->crow_route_length)
-        //                    << " curvature" << setprecision(old_precision) << endl;
-        //            }
-        //            else {
-        //                cout << "not found" << endl;
-        //            }
-        //        }
-        //        if (cmd.first == "Stop"s) {
-        //            string stop_name = cmd.second;
-        //            const auto buses_for_stop = transport_catalogue.GetStopInfo(stop_name);
-        //            cout << "Stop " << stop_name << ": ";
-        //            if (!buses_for_stop.has_value()) {
-        //                cout << "not found" << endl;
-        //                continue;
-        //                //return;
-        //            }
-        //            if (buses_for_stop->size() == 0) {
-        //                cout << "no buses" << endl;
-        //                continue;
-        //                //return;
-        //            }
-        //            cout << "buses";
-        //            for (const auto bus_name : *buses_for_stop) {
-        //                cout << ' ' << bus_name;
-        //            }
-        //            cout << endl;
-        //        }
-        //    }
-        //}
-
         // парсит запросы на получение статистики из потока и печатает результат
         void StatsRequestProcessor::ProcessRequests(
             const TransportCatalogue& transport_catalogue,
