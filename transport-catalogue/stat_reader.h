4fc764b9f007473f990e5415ae3286dce71a6959
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-03-23 16:44:23 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,13 @@
-// место для вашего кода
+#pragma once
+
+#include <istream>
+#include <optional>
+#include <string_view>
+
+#include "transport_catalogue.h"
+
+namespace transport_catalogue::stat_reader {
+  
+    void GetInfo(const TransportCatalogue& transport_catalogue);
+
+}  // namespace transport_catalogue::stat_reader
