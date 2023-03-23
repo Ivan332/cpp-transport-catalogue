d268cc79927d5f4fefe010053b42d5abdddfbe4d
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-03-24 02:29:02 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -7,7 +7,39 @@
 #include "transport_catalogue.h"
 
 namespace transport_catalogue::stat_reader {
-  
-    void GetInfo(const TransportCatalogue& transport_catalogue);
+
+    namespace to_char_stream {
+
+       // класс для печати статистики о маршрутах и остановках в символьный поток
+        class StatsPrinter {
+        public:
+            StatsPrinter(std::ostream& sout) : sout_(sout) {}
+
+            void PrintBusStats(std::string_view bus_name,
+                const std::optional<BusStats>& bus_stats);
+            void PrintStopInfo(std::string_view stop_name,
+                const std::optional<BusesForStop>& stop_info);
+
+        private:
+            std::ostream& sout_;
+        };
+
+    }  // namespace to_char_stream
+
+    namespace from_char_stream {
+
+        // класс-обработчик запросов на получение статистики из транспортного справочника
+        class StatsRequestProcessor {
+        public:
+            StatsRequestProcessor(std::istream& sin) : sin_(sin) {}
+
+            void ProcessRequests(const TransportCatalogue&,
+                to_char_stream::StatsPrinter&);
+
+        private:
+            std::istream& sin_;
+        };
+
+    }  // namespace from_char_stream
 
 }  // namespace transport_catalogue::stat_reader
