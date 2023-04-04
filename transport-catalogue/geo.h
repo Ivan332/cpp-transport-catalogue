d70773a2ea28b72058b06b488f37dac9f8f3f90c
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-04-04 15:36:16 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -4,26 +4,25 @@
 #include <math.h>
 #include <cmath>
 
-struct Coordinates {
-    double lat;
-    double lng;
-    bool operator==(const Coordinates& other) const {
-        return lat == other.lat && lng == other.lng;
-    }
-    bool operator!=(const Coordinates& other) const {
-        return !(*this == other);
-    }
-};
+namespace geo {
+    struct Coordinates {
+        double lat;
+        double lng;
 
-inline static const double EARTH_RADIUS = 6371000.0;
+        bool operator==(const Coordinates& other) const;
+        bool operator!=(const Coordinates& other) const;
+    };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
-    using namespace std;
-    if (from == to) {
-        return 0;
+    inline static const double EARTH_RADIUS = 6371000.0;
+
+    inline double ComputeDistance(Coordinates from, Coordinates to) {
+        using namespace std;
+        if (from == to) {
+            return 0;
+        }
+        static const double dr = M_PI / 180.;
+        return acos(sin(from.lat * dr) * sin(to.lat * dr)
+            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+            * EARTH_RADIUS;
     }
-    static const double dr = M_PI / 180.;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * EARTH_RADIUS;
 }
