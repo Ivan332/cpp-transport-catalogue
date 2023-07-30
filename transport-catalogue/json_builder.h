dfb2a37ae1c18849bdfa676976a389a11686b1eb
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-07-30 04:35:47 +0300

    Update json_builder.h

--- transport-catalogue/json_builder.h
+++ transport-catalogue/json_builder.h
@@ -1,122 +1,93 @@
 #pragma once
-
-#include <algorithm>
-#include <string>
-#include <vector>
-
+ 
 #include "json.h"
-
+#include <stack>
+#include <string>
+#include <memory>
+ 
+namespace transport_catalogue {
+namespace detail {
 namespace json {
-
-	class DictKeyPart;
-	class DictValuePart;
-	class ArrayPart;
-
-	// Конструктор JSON и методы работы с ним
-	class Builder {
-	public:
-		Builder() = default;
-		Builder(Builder&& other);
-		Builder& operator=(Builder&&);
-
-		Builder(const Builder&) = delete;
-		Builder& operator=(const Builder&) = delete;
-
-		Builder& Key(std::string);
-		Builder& Value(Node::Value);
-		DictKeyPart StartDict();
-		Builder& EndDict();
-		ArrayPart StartArray();
-		Builder& EndArray();
-		Node Build();
-
-	private:
-		// Верхняя нода в стеке - текущий собираемый массив или словарь.
-		std::vector<Node> stack_;
-
-		// `true`, если значения идут в словарь 
-		bool expect_key_ = false;
-
-		// `true` после сборки ноды верхнего уровня
-		bool finished_ = false;
-
-		// Стек названий ключей во вложенных собираемых словарях
-		std::vector<std::string> key_stack_;
-
-		// `true`, если значение данного конструктора было перемещено в другую переменную
-		bool moved_out_of_ = false;
-
-		void Swap(Builder& other);
-	};
-
-	// Общий класс для вспомогательных классов - конструкторов частей JSON нод
-	class PartBuilder {
-	protected:
-		PartBuilder(Builder&& builder) 
-			: builder_(std::move(builder)) {
-		}
-		Builder builder_;
-
-		Builder& Key(std::string);
-		Builder& Value(Node::Value);
-		DictKeyPart StartDict();
-		Builder& EndDict();
-		ArrayPart StartArray();
-		Builder& EndArray();
-		Node Build();
-
-	};
-
-	// Часть конструктора JSON ( `Key()` - `Value()` / окончание словаря )
-	class DictKeyPart : private PartBuilder {
-		using PartBuilder::PartBuilder;
-
-	public:
-		DictKeyPart(Builder&& builder)
-			: PartBuilder(std::move(builder)) {
-		}
-
-		DictKeyPart(const DictKeyPart&) = delete;
-		DictKeyPart& operator=(const DictKeyPart&) = delete;
-
-		DictValuePart Key(std::string);
-		Builder EndDict();
-	};
-
-	// Часть конструктора JSON ( `Value()` / начало вложенного словаря или массива )
-	class DictValuePart : private PartBuilder {
-		using PartBuilder::PartBuilder;
-
-	public:
-		DictValuePart(Builder&& builder)
-			: PartBuilder(std::move(builder)) {
-		}
-
-		DictValuePart(const DictValuePart&) = delete;
-		DictValuePart& operator=(const DictValuePart&) = delete;
-		DictValuePart& operator=(DictValuePart&&) = delete;
-
-		DictKeyPart Value(Node::Value);
-		DictKeyPart StartDict();
-		ArrayPart StartArray();
-	};
-
-	// Часть конструктора JSON ( элемент массива / окончание массива )
-	class ArrayPart : private PartBuilder {
-		using PartBuilder::PartBuilder;
-
-	public:
-		ArrayPart(Builder&& builder)
-			: PartBuilder(std::move(builder)) {
-		}
-
-		ArrayPart(const ArrayPart&) = delete;
-		ArrayPart& operator=(const ArrayPart&) = delete;
-
-		ArrayPart& Value(Node::Value);
-		DictKeyPart StartDict();
-		ArrayPart StartArray();
-		Builder EndArray();
-	};
-
-}  // namespace json
+namespace builder {
+ 
+class KeyContext;
+class DictionaryContext;
+class ArrayContext;
+ 
+class Builder {
+public:
+    Node make_node(const Node::Value& value_);
+    void add_node(const Node& node);
+ 
+    KeyContext key(const std::string& key_);
+    Builder& value(const Node::Value& value);
+    
+    DictionaryContext start_dict();
+    Builder& end_dict();
+    
+    ArrayContext start_array();
+    Builder& end_array();
+ 
+    Node build();
+ 
+private:
+    Node root_;
+    std::vector<std::unique_ptr<Node>> nodes_stack_;
+};
+ 
+class BaseContext {
+public:
+    BaseContext(Builder& builder);
+ 
+    KeyContext key(const std::string& key);
+    Builder& value(const Node::Value& value);
+    
+    DictionaryContext start_dict();
+    Builder& end_dict();
+    
+    ArrayContext start_array();
+    Builder& end_array();
+ 
+protected:
+    Builder& builder_;
+};
+ 
+class KeyContext : public BaseContext {
+public:
+    KeyContext(Builder& builder);
+ 
+    KeyContext key(const std::string& key) = delete;
+ 
+    BaseContext end_dict() = delete;
+    BaseContext end_array() = delete;
+ 
+    DictionaryContext value(const Node::Value& value);
+};
+ 
+class DictionaryContext : public BaseContext {
+public:
+    DictionaryContext(Builder& builder);
+ 
+    DictionaryContext start_dict() = delete;
+ 
+    ArrayContext start_array() = delete;
+    Builder& end_array() = delete;
+ 
+    Builder& value(const Node::Value& value) = delete;
+};
+ 
+class ArrayContext : public BaseContext {
+public:
+    ArrayContext(Builder& builder);
+ 
+    KeyContext key(const std::string& key) = delete;
+ 
+    Builder& end_dict() = delete;
+ 
+    ArrayContext value(const Node::Value& value);
+};
+ 
+} // namespace builder
+} // namespace json
+} // namespace detail
+} // namespace transport_catalogue
