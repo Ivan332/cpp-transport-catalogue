27fca8b36b99bb0b4918d238acc657890d7a4e23
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-07-30 18:47:19 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -15,14 +15,14 @@ namespace geo {
 
     inline static const double EARTH_RADIUS = 6371000.0;
 
-    inline double compute_distance(Coordinates from, Coordinates to) {
+    inline double ComputeDistance(Coordinates from, Coordinates to) {
         using namespace std;
         if (from == to) {
             return 0;
         }
         static const double dr = M_PI / 180.;
-        return acos(sin(from.lat * dr) * sin(to.lat * dr)
-            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        return acos(sin(from.latitude * dr) * sin(to.latitude * dr)
+            + cos(from.latitude * dr) * cos(to.latitude * dr) * cos(abs(from.longitude - to.longitude) * dr))
             * EARTH_RADIUS;
     }
 } // namespace geo
